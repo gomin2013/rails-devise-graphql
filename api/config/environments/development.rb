d992447867d3c9d29ed234ca204b28044881582b
watthajak2018 <watthajak@abroaders.jp>
2020-02-15 01:54:24 +0700

    Setup the Docker.

--- api/config/environments/development.rb
+++ api/config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << 'dev.nuxt-on-rails'
+  config.hosts << 'dev.rails-devise-graphql'
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
