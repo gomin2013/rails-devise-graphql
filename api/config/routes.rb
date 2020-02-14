1af82496fb09c47b68cc8979cb3077778e8d8633
watthajak2018 <watthajak@abroaders.jp>
2020-02-15 01:54:52 +0700

    Generated the 'Rails API' app.

---
+++ api/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: 'graphql#execute' if Rails.env.development?
+
+  post "/graphql", to: "graphql#execute"
+
+  devise_for :users
+end
---
+++ api/db/seeds.rb
@@ -0,0 +1,7 @@
+User.create(
+  email: 'user+1@example.com',
+  password: 'password',
+  password_confirmation: 'password',
+  first_name: 'FirstName',
+  last_name: 'LastName'
+)
